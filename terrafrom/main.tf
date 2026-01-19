42661327be2ff2bf2e32885d3a92d720fc531701
ravi-ka-hcl <ravi-ka@hcltech.com>
2026-01-19 11:56:35 +0530

    Create main.tf

---
+++ terrafrom/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"  # pick your preferred major version
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}

25941f9f89f69f985c471746083efd4ac6d0df62
ravi-ka-hcl <ravi-ka@hcltech.com>
2026-01-19 14:03:02 +0530

    Delete terraform/variables.tf

--- terraform/variables.tf
+++
@@ -1,6 +0,0 @@
-
-# variables.tf
-variable "subscription_id" { type = string }
-variable "tenant_id"       { type = string }
-variable "client_id"       { type = string }
-variable "client_secret"   { type = string }

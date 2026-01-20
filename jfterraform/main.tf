dfd1377dcf482a48b4de5e606bf9c2aaa50290e0
ravi-ka-hcl <ravi-ka@hcltech.com>
2026-01-20 13:13:16 +0530

    Updates main.tf

--- jfterraform/main.tf
+++ jfterraform/main.tf
@@ -1,21 +1,28 @@
 terraform {
   backend "remote" {
-    hostname     = "<YOUR_JFROG_URL>"
-    organization = "<YOUR_ARTIFactory_REPO_KEY>" # Repository name
+    hostname     = "https://trialpf4dwg.jfrog.io/artifactory/tempstroage-generic-local/"
+    organization = "tempstroage-generic-local" # Repository name
     # The workspace name will be used as the prefix for the state file in Artifactory
     # For example, a workspace named "prod" will store the state at <repo_key>/prod/terraform.tfstate
-    prefix       = "env/prod" 
+    prefix       = "dev" 
   }
 
+  terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 3.0"
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  # Authentication will be handled by environment variables in GitHub Actions
+}
+
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "jf-resources"
+  location = "East US"
 }

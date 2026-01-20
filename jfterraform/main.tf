10e2f7dae36c65c40eba5598f37c0eb90ccce7c4
ravi-ka-hcl <ravi-ka@hcltech.com>
2026-01-20 20:37:45 +0530

    Update main.tf

--- jfterraform/main.tf
+++ jfterraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
-  backend "remote" {
-    hostname     = "https://trialpf4dwg.jfrog.io/artifactory/tempstroage-generic-local/"
+  backend "artifactory" {
+    hostname     = "https://trialpf4dwg.jfrog.io/artifactory"
     organization = "tempstroage-generic-local" # Repository name
     # The workspace name will be used as the prefix for the state file in Artifactory
     # For example, a workspace named "prod" will store the state at <repo_key>/prod/terraform.tfstate

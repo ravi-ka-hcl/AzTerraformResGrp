d05ded14f2341d2c8fba10589f3246b3366fc9ac
ravi-ka-hcl <ravi-ka@hcltech.com>
2026-01-21 13:46:53 +0530

    Create backend.tf

---
+++ terraform-artifactory-remote-demo/backend.tf
@@ -0,0 +1,24 @@
+
+terraform {
+  required_version = ">= 1.4.0"
+
+  # Use JFrog Artifactory as a Terraform Remote Backend
+  backend "remote" {
+    # Your JFrog domain (no protocol)
+    hostname     = "trialpf4dwg.jfrog.io"
+
+    # The Artifactory Terraform Backend repository key
+    organization = "tf-backend"
+
+    # Choose one option:
+    # A) Use a single, explicit workspace
+    workspaces {
+      name = "demo-dev"
+    }
+
+    # B) Or, use a prefix to map multiple workspaces (uncomment instead of 'name'):
+    # workspaces {
+    #   prefix = "demo-"
+    # }
+  }
+}
